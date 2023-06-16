bf246cd8b44d278f32ccfcf1ca5ebc883649bf69
yaktas <yunusemreaktas97@gmail.com>
2023-06-16 20:08:25 +0300

    main little line fixed

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,9 +5,12 @@ int main() {
     std::string *stringPTR = &brain;
     std::string &stringREF = brain;
 
-    std::cout << "string adress : " << &brain << "\nstringptr adress : " << &stringPTR << std::endl;
+    std::cout << std::endl;
+    std::cout << "string adress    : " << &brain << std::endl;
+    std::cout << "stringptr adress : " << &stringPTR << std::endl;
     std::cout << "stringREF adress : " << &stringREF << std::endl;
     std::cout << std::endl;
-    std::cout << "string value : " << brain << "\nstringptr value : " << *stringPTR << std::endl;
-    std::cout << "stringREF value : " << stringREF << std::endl;
+    std::cout << "string value     : " << brain << std::endl;
+    std::cout << "stringptr value  : " << *stringPTR << std::endl;
+    std::cout << "stringREF value  : " << stringREF << std::endl;
 } 
